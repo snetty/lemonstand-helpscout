8c36e72a1817bedf8f24088ddffcf2fbf1971fda
Simon Stevens <simon@3dpixel.net>
2014-11-27 10:25:54 +0000

    removed api key (so dumb)

--- classes/helpscout_module.php
+++ classes/helpscout_module.php
@@ -3,7 +3,7 @@ use HelpScoutApp\DynamicApp;
 
 class Helpscout_Module extends Core_ModuleBase {
 
-	private $helpscout_api_key = '998asudhajks298q234rjds0a8sufaskd';
+	private $helpscout_api_key = '';
 
 	protected function createModuleInfo() {
 		return new Core_ModuleInfo(
@@ -22,26 +22,26 @@ class Helpscout_Module extends Core_ModuleBase {
 	public function helpscout_callback() {
 		$app = new DynamicApp($this->helpscout_api_key);
 		if ($app->isSignatureValid()) {
-			try{
+			try {
 				$customer = Shop_Customer::create()->find_by_email($app->getCustomer()->getEmail());
 				if (!$customer) {
 					throw new Exception('Customer not found for <strong>' . $app->getCustomer()->getEmail() . '</strong>');
 				}
-				
+
 				$paid_orders = new Db_DataCollection();
 				foreach ($customer->orders as $order) {
 					if ($order->is_paid()) {
 						$paid_orders[] = $order;
 					}
 				}
-				
+
 				$filename = PATH_APP . '/modules/helpscout/partials/helpscout.htm';
 				ob_start();
-				include($filename);
+				include $filename;
 				$html = ob_get_contents();
-				ob_end_clean ();
-			} catch (Exception $e){
-				ob_end_clean ();
+				ob_end_clean();
+			} catch (Exception $e) {
+				ob_end_clean();
 				$html = '<ul class="unstyled"><li>' . $e->getMessage() . '</li></ul>';
 			}
 			echo $app->getResponse($html);
